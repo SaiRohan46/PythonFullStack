9dc5b297b3b8adb60ff2334d4c1de84e493e3b16
SaiRohan46 <rohansai4645@gmail.com>
2026-05-13 15:32:39 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,43 @@
+'''
+num = 23
+any =[1,2]
+an = {"Name": "Rohan"}
+print(num)
+print(any)
+print(an)
+
+-------
+TOKENS
+---> keywords in the program
+if
+else
+while
+for
+-------
+IDENTIFIERS
+---> names of variables functions and classes
+-------
+VARIABLES
+GOOD WAYS :
+A-Z,a-z,_
+BAD WAYS:
+special char,spaces,0-9
+--------
+COMMENTS
+1.Single Line comment(#)
+2.Multi line comment(triples quotes)
+
+#Create variables of 3 types 
+a=54
+4b=7777 --- this gives invalid string lteral as error 
+^h=6434 --- this gives syntax error
+print(a)
+print(b)
+print(^h)--- codes doesnt execute until here
+'''
+a=5
+b=6
+print(a == b)
+
+
+
